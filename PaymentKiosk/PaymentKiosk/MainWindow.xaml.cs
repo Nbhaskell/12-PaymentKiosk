0a9587fcad3a4da1a0d40b80691208ba320f9f96
Nathan Haskell <Nathan Haskell>
2016-01-29 12:09:56 -0800

    Updated 2nd window styling a little bit.

--- PaymentKiosk/PaymentKiosk/MainWindow.xaml.cs
+++ PaymentKiosk/PaymentKiosk/MainWindow.xaml.cs
@@ -18,9 +18,6 @@ using Twilio;
 
 namespace PaymentKiosk
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
